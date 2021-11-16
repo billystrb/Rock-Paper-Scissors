c55ce30511e3459acbdc523fed8fcc9be99e749e
billystrb <billairbourne@gmail.com>
2021-11-16 08:26:29 +0200

    Added comments

--- rps.js
+++ rps.js
@@ -1,91 +1,99 @@
-    let options = ["rock","paper","scissors"]
-    let x = " "
-    function comupterPlay(x){
-        computersOption = Math.floor(Math.random() * options.length )
-        return options[computersOption]
-    }
-    let computersChoice = comupterPlay(x)
-    console.log(computersChoice)
+// Generate random pick for the computer 
+let options = ["rock","paper","scissors"]
+let x = " "
+function comupterPlay(x){
+    computersOption = Math.floor(Math.random() * options.length )
+    return options[computersOption]
+}
+let computersChoice = comupterPlay(x)
+//  ------------------------------------
 
-    let wraper = document.querySelector('#playerChoice')
+// create an object to add the results on 
+let wraper = document.querySelector('#playerChoice')
+// ------------------------------------------
 
-    let rockButton = document.querySelector('#rock')
-    rockButton.addEventListener('click',rockPlayround)
-    function rockPlayround() {
-        let theResult = playround("rock",computersChoice)
-        displayResult(theResult,'rock')
+// Any button selected must 1. display your choice, 
+// 2. return computer's choice ,3. calcualte the winner
+let rockButton = document.querySelector('#rock')
+rockButton.addEventListener('click',rockPlayround)
+function rockPlayround() {
+    let theResult = playround("rock",computersChoice)
+    displayResult(theResult,'rock')
+}
 
-    }
+let paperButton = document.querySelector('#paper')
+paperButton.addEventListener('click',paperPlayround)
+function paperPlayround() {
+    let theResult = playround("paper",computersChoice)
+    displayResult(theResult,'paper')
+}
 
-    let paperButton = document.querySelector('#paper')
-    paperButton.addEventListener('click',paperPlayround)
-    function paperPlayround() {
-        let theResult = playround("paper",computersChoice)
-        displayResult(theResult,'paper')
+let scissorsButton = document.querySelector('#scissors')
+scissorsButton.addEventListener('click',scissorsPlayround)
+function scissorsPlayround() {
+    let theResult = playround("scissors",computersChoice)
+    displayResult(theResult,'scissors')
+}
 
+// ------------------------------------------------------------
 
-    }
+// Find the winner of the game 
+let playersChoice 
 
-    let scissorsButton = document.querySelector('#scissors')
-    scissorsButton.addEventListener('click',scissorsPlayround)
-    function scissorsPlayround() {
-        let theResult = playround("scissors",computersChoice)
-        displayResult(theResult,'scissors')
-
-    }
-
-
-    let playersChoice 
-
-    function playround(playersChoice,computersChoice){
-        switch (playersChoice){
-            case "rock" :
-                if(computersChoice == "rock"){
-                    return 'tie'
-                }else if(computersChoice == "scissors"){
-                    return 'rock'
-                }else{
-                    return 'paper'
-                }
-            case "paper" :
-                if(computersChoice == "rock"){
-                    return 'paper'
-                }else if(computersChoice == "scissors"){
-                    return 'scissors'
-                }else{
-                    return 'tie'
-                }
-            case "scissors" :
-                if(computersChoice == "rock"){
-                    return 'rock'
-                }else if(computersChoice == "scissors"){
-                    return 'tie'
-                }else{
-                    return 'scissors'
-                }
-
-        }
-    }
-
-    function displayResult(aResult,playersPlay){
-        let computersPlay = document.createElement('div')
-        let playerPlays = document.createElement('div')
-        playerPlays.textContent = 'You chose ' + playersPlay
-        computersPlay.textContent = 'The computer picks ' + computersChoice
-        wraper.appendChild(playerPlays)
-        wraper.appendChild(computersPlay)
-        displayWinner(aResult,computersChoice,playersPlay)
+function playround(playersChoice,computersChoice){
+    switch (playersChoice){
+        case "rock" :
+            if(computersChoice == "rock"){
+                return 'tie'
+            }else if(computersChoice == "scissors"){
+                return 'rock'
+            }else{
+                return 'paper'
+            }
+        case "paper" :
+            if(computersChoice == "rock"){
+                return 'paper'
+            }else if(computersChoice == "scissors"){
+                return 'scissors'
+            }else{
+                return 'tie'
+            }
+        case "scissors" :
+            if(computersChoice == "rock"){
+                return 'rock'
+            }else if(computersChoice == "scissors"){
+                return 'tie'
+            }else{
+                return 'scissors'
+            }
 
     }
+}
 
-    function displayWinner(aResult,npc,pc){
-        if (pc == npc ){
-            let result = document.createElement('div')
-            result.textContent = ' Its a tie '
-            wraper.appendChild(result)
-        }else {
-            let result = document.createElement('div')
-            result.textContent = ' Winner is ' + aResult 
-            wraper.appendChild(result)
-        }
+// --------------------------------------
+
+// create divs and fill them with both the players and the computers choice
+function displayResult(aResult,playersPlay){
+    let computersPlay = document.createElement('div')
+    let playerPlays = document.createElement('div')
+    playerPlays.textContent = 'You chose ' + playersPlay
+    computersPlay.textContent = 'The computer picks ' + computersChoice
+    wraper.appendChild(playerPlays)
+    wraper.appendChild(computersPlay)
+    displayWinner(aResult,computersChoice,playersPlay)
+}
+// -------------------------------------------
+
+// Display the winner altering the answer in case of a tie 
+function displayWinner(aResult,npc,pc){
+    if (pc == npc ){
+        let result = document.createElement('div')
+        result.textContent = ' Its a tie '
+        wraper.appendChild(result)
+    }else {
+        let result = document.createElement('div')
+        result.textContent = ' Winner is ' + aResult 
+        wraper.appendChild(result)
     }
+}
+// -----------------------------------------------
